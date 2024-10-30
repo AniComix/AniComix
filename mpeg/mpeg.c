df152f82eba3ff98a2ba9a18c4dd9bad31b5c86f
deltamaya <deltamaya553@gmail.com>
2024-10-30 20:52:56 +0800

    initial commit

---
+++ mpeg/mpeg.c
@@ -0,0 +1,5 @@
+//
+// Created by delta on 30 Oct 2024.
+//
+
+#include "mpeg.h
---
+++ mpeg/mpeg.h
@@ -0,0 +1,10 @@
+//
+// Created by delta on 30 Oct 2024.
+//
+
+#ifndef MPEG__MPEG_H_
+#define MPEG__MPEG_H_
+
+
+
+#endif //MPEG__MPEG_H_
